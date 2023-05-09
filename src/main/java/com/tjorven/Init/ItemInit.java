e112fbfb9240bd8af3dcdacd86cbc118f34bc56a
Tjorven Grawunder <tjorven.grawunder@gmail.com>
2023-05-09 10:38:20 +0200

    New Crafting Recipe for Gold

--- src/main/java/com/tjorven/Init/ItemInit.java
+++ src/main/java/com/tjorven/Init/ItemInit.java
@@ -1,6 +1,7 @@
 package com.tjorven.Init;
 
 import com.tjorven.Items.ItemRaw;
+import com.tjorven.Items.ItemTrader;
 import net.minecraft.item.Item;
 
 import java.util.ArrayList;
@@ -11,6 +12,6 @@ public class ItemInit {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
     public static final Item SPECIAL_INGOT = new ItemRaw("ingot_special");
-    public static final Item TRADERS_BOWL = new ItemRaw("item_trader");
+    public static final Item TRADERS_BOWL = new ItemTrader("item_trader");
     public static final Item GREEN_APPLE = new ItemRaw("food_gapple");
 }
---
+++ src/main/java/com/tjorven/Items/ItemTrader.java
@@ -0,0 +1,12 @@
+package com.tjorven.Items;
+
+import net.minecraft.item.ItemStack;
+
+public class ItemTrader extends ItemRaw{
+    public ItemTrader(String name) {
+        super(name);
+        this.setContainerItem(this);
+        this.setMaxStackSize(1);
+    }
+
+}
